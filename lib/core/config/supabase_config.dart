feff698153c48dead8106804564f845eb8a0ad79
LuisCuax <diegoarroyo911@gmial.com>
2025-11-24 18:10:43 -0600

    feat(supabase): update project credentials

--- lib/core/config/supabase_config.dart
+++ lib/core/config/supabase_config.dart
@@ -1,6 +1,6 @@
 class SupabaseConfig {
   // TODO: Replace with your actual Supabase URL and Anon Key
-  static const String url = 'https://mwldonzgeruhrsfirfop.supabase.co';
+  static const String url = 'https://ljvuzypqigsxcpzuojmy.supabase.co';
   static const String anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13bGRvbnpnZXJ1aHJzZmlyZm9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MTYwMzAsImV4cCI6MjA3ODQ5MjAzMH0.ZefZzLerTtke3rj3lD1DItLVbcBkUMBZ65p98Hk2H6w';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqdnV6eXBxaWdzeGNwenVvam15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5Mjc2MTIsImV4cCI6MjA3OTUwMzYxMn0.oygsRxv_Mmxr0TZqWFewDKhNyyd5fkn2kJvLzaC9JWE';
 }
